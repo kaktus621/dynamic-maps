9a837699967f533119391e2c7fcdb5ca975d9fd7
Martin Matysiak <kaktus621@gmail.com>
2012-09-02 14:22:14 +0200

    Adding ClickthroughHandler class. Signed-off-by: Martin Matysiak <kaktus621@gmail.com>

---
+++ clickthrough.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview Main script that will be injected to Google+.
+ * @author kaktus621@gmail.com (Martin Matysiak)
+ */
+
+
+
+/**
+ * The main class that will be instantiated on load.
+ * @constructor
+ */
+ClickthroughHandler = function() {
+  // TODO(kaktus621): Late-load Maps API, attach handlers to static maps images.
+};
